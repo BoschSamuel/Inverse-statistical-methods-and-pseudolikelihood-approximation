51a6748b8631c388e9061dd17f1616c05a625906
Samuel Bosch <samuel.bosch@epfl.ch>
2018-10-11 22:49:57 +0200

    Initial Commit

---
+++ Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Inverse-statistical-methods-and-pseudolikelihood-approximation
+//
+//  Created by Samuel Bosch on 11/10/18.
+//  Copyright © 2018 Samuel Bosch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
