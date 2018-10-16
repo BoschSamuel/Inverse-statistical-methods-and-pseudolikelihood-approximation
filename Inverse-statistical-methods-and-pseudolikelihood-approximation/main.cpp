37b09ab57debae44f658e5a15a80723601ba6e09
Samuel Bosch <samuelbosch@MacBosch-Pro.local>
2018-10-16 18:27:17 +0200

    Fixed the big I had which initialized the total energy to E=0, every time a spin was gessed at the same value as before

--- Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
+++ Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
@@ -21,12 +21,12 @@ int main(int argc, const char * argv[]) {
     int N = 20; //Number of atoms in molecule
     double T = 300; // This is the temperature of the experiment
     // double K_b = 1.38064852e-23; // Boltzman constant
-    double K_b = 0.01;
+    double K_b = 0.008;
     uniform_int_distribution<int> random_spin(min_spin,max_spin); // definition of random function
     uniform_int_distribution<int> random_atom(0,N-1); // definition of random function
     
     // Random initialization of spins
-    cout << "Initial random distribution:\n";
+    //cout << "Initial random distribution:\n";
     vector<int> v(N);    // declares a vector of integers
     for(int i=0; i<N; i++){
         auto random_integer = random_spin(rng);
@@ -35,7 +35,7 @@ int main(int argc, const char * argv[]) {
     }
     
     // Calculation of the energy using Pott's model (H = -J*sum(Kronecker_delta(i,j))
-    cout << "\nTotal energy = ";
+    //cout << "\nTotal energy = ";
     int E = 0;
     for(int i=0; i<N; i++){
         for(int j=i+1; j<N; j++){
@@ -46,18 +46,20 @@ int main(int argc, const char * argv[]) {
     }
     cout << E << "\n\n";
 
-    cout << "\nStarting random iterations...\n";
+    //cout << "\nStarting random iterations...\n";
     int max_number_of_interations = 10000;
     for(int iteration_number=0; iteration_number<max_number_of_interations; iteration_number++){
         auto atom_number = random_atom(rng); //Pick random atom for changing the spin
         int old_spin = v[atom_number]; //Saving old spin in case we still want to use it
+        //cout << "Atom is = " << atom_number << " Spin was = " << old_spin;
         int E_old = E;
-        E = 0;
         v[atom_number] = random_spin(rng); //Pick random new spin for selected atom
+        //cout << " and now is = " << v[atom_number] << '\n';
         if(v[atom_number] == old_spin){
             continue; // If the spin didn't change, we do nothing
         }
         // Calculation of the new energy
+        E = 0;
         for(int i=0; i<N; i++){
             for(int j=i+1; j<N; j++){
                 if(v[i] == v[j]){
@@ -65,15 +67,28 @@ int main(int argc, const char * argv[]) {
                 }
             }
         }
+        //cout << "Energy was = " << E_old << " and now is = " << E << '\n';
         if(E > E_old){
-            double p = (double)rand()/(double)RAND_MAX;
-            if (exp(-(E-E_old)/(K_b*T))<p){
+            //cout << "Energy want to increase\n";
+            double uniform_random_0_1 = (double)rand()/(double)RAND_MAX;
+            double prob = exp(-(E-E_old)/(K_b*T));
+            if (prob < uniform_random_0_1){
                 // With some small probability, we revert the change (Metropolis algorithm)
                 E = E_old;
                 v[atom_number] = old_spin;
             }
+            //cout << prob << ' ' << uniform_random_0_1 << '\n';
+        }else{
+            //cout << "Energy decreased\n";
         }
+        //cout << "Energy now is = " << E << "\n\n";
         cout << E << '\n';
+    }
+    
+    
+    cout << "Final spin configuration:\n";
+    for(int i=0; i<N; i++){
+        cout << v[i] << ' ';
     }
     
     
