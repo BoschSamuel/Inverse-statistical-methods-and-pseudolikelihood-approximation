ca98dcbc21816349eba332e141eb93e745c6fcc0
Samuel Bosch <samuelbosch@MacBosch-Pro.local>
2018-10-16 16:52:50 +0200

    Initial attempt of the Monte-Carlo and Metropolis algorithm

--- Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
+++ Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
@@ -16,21 +16,26 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     random_device rd;     // only used once to initialise (seed) engine
     mt19937 rng(rd());    // random-number engine used (Mersenne-Twister in this case)
-    int min = 0;
-    int max = 21;
-    int N = 20;
-    uniform_int_distribution<int> uni(min,max); // guaranteed unbiased
+    int min_spin = 0;  // min value spin can take
+    int max_spin = 21; // max value spin can take
+    int N = 20; //Number of atoms in molecule
+    double T = 300; // This is the temperature of the experiment
+    // double K_b = 1.38064852e-23; // Boltzman constant
+    double K_b = 0.01;
+    uniform_int_distribution<int> random_spin(min_spin,max_spin); // definition of random function
+    uniform_int_distribution<int> random_atom(0,N-1); // definition of random function
     
+    // Random initialization of spins
     cout << "Initial random distribution:\n";
     vector<int> v(N);    // declares a vector of integers
     for(int i=0; i<N; i++){
-        auto random_integer = uni(rng);
+        auto random_integer = random_spin(rng);
         cout << random_integer << ' ';
         v[i] = random_integer;
     }
     
-    
-    cout << "\n\nTotal energy = ";
+    // Calculation of the energy using Pott's model (H = -J*sum(Kronecker_delta(i,j))
+    cout << "\nTotal energy = ";
     int E = 0;
     for(int i=0; i<N; i++){
         for(int j=i+1; j<N; j++){
@@ -39,9 +44,37 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
-    cout << E;
-    
-    
+    cout << E << "\n\n";
+
+    cout << "\nStarting random iterations...\n";
+    int max_number_of_interations = 10000;
+    for(int iteration_number=0; iteration_number<max_number_of_interations; iteration_number++){
+        auto atom_number = random_atom(rng); //Pick random atom for changing the spin
+        int old_spin = v[atom_number]; //Saving old spin in case we still want to use it
+        int E_old = E;
+        E = 0;
+        v[atom_number] = random_spin(rng); //Pick random new spin for selected atom
+        if(v[atom_number] == old_spin){
+            continue; // If the spin didn't change, we do nothing
+        }
+        // Calculation of the new energy
+        for(int i=0; i<N; i++){
+            for(int j=i+1; j<N; j++){
+                if(v[i] == v[j]){
+                    E = E-1;
+                }
+            }
+        }
+        if(E > E_old){
+            double p = (double)rand()/(double)RAND_MAX;
+            if (exp(-(E-E_old)/(K_b*T))<p){
+                // With some small probability, we revert the change (Metropolis algorithm)
+                E = E_old;
+                v[atom_number] = old_spin;
+            }
+        }
+        cout << E << '\n';
+    }
     
     
     cout << '\n';
