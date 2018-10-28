8d4e79336162018d160fd776cd3c3f086b966692
Samuel Bosch <samuelbosch@MacBosch-Pro.local>
2018-10-28 18:34:06 +0100

    Tried to fix the energy function problems without success...

--- Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
+++ Inverse-statistical-methods-and-pseudolikelihood-approximation/main.cpp
@@ -32,23 +32,20 @@ int main(int argc, const char * argv[]) {
     array<std::array<double, 525>, 525> J;
     ifstream myfile;
     myfile.open("/Users/samuelbosch/OneDrive/Faks/EPFL_M1/Computer_simulation_of_physical_systems/Project/Inverse-statistical-methods-and-pseudolikelihood-approximation/J.txt");
-    if(!myfile) //Testing if file is actually open.
-    {
+    if(!myfile){ //Testing if file is actually open.
         cout << "Error opening output file" << endl;
         return -1;
     }else{
-        cout << "File 'J.txt' is open" << '\n';
+        cout << "File 'J.txt' is open" << "\n\n";
     }
     for (int i = 0; i < 525; i++){
         for(int j = 0; j < 525; ++j){
             myfile >> J[i][j]; // Here we read the file number by number
-            // cout << J[i][j] << '\t';
         }
-        // cout << endl;
     }
 
     // Random initialization of spins
-    //cout << "Initial random distribution:\n";
+    cout << "Initial spin configuration:\n";
     vector<int> v(N);    // declares a vector of integers
     for(int i=0; i<N; i++){
         auto random_integer = random_spin(rng);
@@ -57,34 +54,28 @@ int main(int argc, const char * argv[]) {
     }
 
     // Calculation of the energy using Pott's model (H = -J*sum(Kronecker_delta(i,j))
-    //cout << "\nTotal energy = ";
     double E = 0.0;
     for(int i=0; i<N; i++){
         for(int j=i+1; j<N; j++){
-            // if(v[i] == v[j]){
-            //    E = E-1;
-            // }
             if (abs(J[21*i+v[i]][21*j+v[j]]) > 0){
-                E = E - J[21*i+v[i]][21*j+v[j]];
-                // cout << J[21*i+v[i]][21*j+v[j]] << '\n';
+                E = E - J[21*i+v[i]][21*j+v[j]]; // Is this the right formula for calculating the energy?
             }
         }
     }
     cout << "\nEnergy(initial)" << " = " << E << "\n\n";
 
     //cout << "\nStarting random iterations...\n";
-    int max_number_of_interations = 100;
-    for(int iteration_number=0; iteration_number<max_number_of_interations; iteration_number++){
+    int max_number_of_interations = 10000;
+    int iteration_number = 0;
+    for(; iteration_number<max_number_of_interations; iteration_number++){
         auto atom_number = random_atom(rng); //Pick random atom for changing the spin
         int old_spin = v[atom_number]; //Saving old spin in case we still want to use it
-        //cout << "Atom is = " << atom_number << " Spin was = " << old_spin;
         double E_old = E;
         v[atom_number] = random_spin(rng); //Pick random new spin for selected atom
-        //cout << " and now is = " << v[atom_number] << '\n';
         if(v[atom_number] == old_spin){
             continue; // If the spin didn't change, we do nothing
         }
-        // Calculation of the new energy
+        // Calculation of the new energy (new, more efficient algorithm)
         for(int i=0; i<N; i++){
             if (atom_number==i){
                 continue;
@@ -97,9 +88,7 @@ int main(int argc, const char * argv[]) {
             }
         }
 
-        //cout << "Energy was = " << E_old << " and now is = " << E << '\n';
         if(E > E_old){
-            //cout << "Energy want to increase\n";
             double uniform_random_0_1 = (double)rand()/(double)RAND_MAX;
             double prob = exp(-(E-E_old)/(K_b*T));
             if (prob < uniform_random_0_1){
@@ -107,18 +96,16 @@ int main(int argc, const char * argv[]) {
                 E = E_old;
                 v[atom_number] = old_spin;
             }
-            //cout << prob << ' ' << uniform_random_0_1 << '\n';
-        }else{
-            //cout << "Energy decreased\n";
         }
-        cout << "Energy(" << iteration_number << ")" << " = " << E << "\n";
     }
 
     cout << "Final spin configuration:\n";
     for(int i=0; i<N; i++){
         cout << v[i] << ' ';
     }
-
+    cout << endl;
+    cout << "Energy(" << iteration_number << ")" << " = " << E << "\n";
     cout << '\n';
+    
     return 0;
 }
